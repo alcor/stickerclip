996cc983ae86fff37cec1d5676c42f56c422b020
Nicholas Jitkoff <nicholas@jitkoff.com>
2022-07-13 23:16:12 -0700

    Add s/m/l selector

--- ClipboardSticker MessagesExtension/MessagesViewController.swift
+++ ClipboardSticker MessagesExtension/MessagesViewController.swift
@@ -8,12 +8,239 @@
 import UIKit
 import Messages
 
+class MessagesViewController: MSMessagesAppViewController, MSStickerBrowserViewDataSource {
+  @IBOutlet weak var stickerView: MSStickerView!
+  @IBOutlet  var stickerBrowser: MSStickerBrowserView!
+  @IBOutlet weak var reloadButton: UIButton!
+  @IBOutlet weak var historyButton: UIButton!
+  @IBOutlet weak var infoLabel: UITextView!
+  @IBOutlet weak var containerView: UIView!
+  @IBOutlet weak var toolbarView: UIView!
+  
+  lazy var files: Array<URL> = {
+    return loadSortedFiles()
+  }()
+  var stickerFile: URL?
+  let store = UserDefaults.standard
+  var showBorder: Bool
+  var stickers: Array<MSSticker> = []
+  var content: UIImage?
+
+  required init?(coder: NSCoder) {
+    showBorder = (store.object(forKey: "showBorder") != nil) ? store.bool(forKey: "showBorder") : true;
+    super.init(coder: coder);
+  }
+  
+  
+  func loadSortedFiles() -> Array<URL> {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    guard let directoryURL = URL(string: paths.path) else {return []}
+    do {
+       return try
+       FileManager.default.contentsOfDirectory(at: directoryURL,
+              includingPropertiesForKeys:[.contentModificationDateKey],
+              options: [.skipsHiddenFiles, .skipsSubdirectoryDescendants])
+           .sorted(by: {
+               let date0 = try $0.promisedItemResourceValues(forKeys:[.contentModificationDateKey]).contentModificationDate!
+               let date1 = try $1.promisedItemResourceValues(forKeys:[.contentModificationDateKey]).contentModificationDate!
+               return date0.compare(date1) == .orderedDescending
+            })
+    } catch {
+        print (error)
+    }
+    return [];
+  }
+  func numberOfStickers(in stickerBrowserView: MSStickerBrowserView) -> Int {
+    return files.count;
+  }
+
+  func stickerBrowserView(_ stickerBrowserView: MSStickerBrowserView, stickerAt index: Int) -> MSSticker {
+    let url = files[index];
+    let sticker =  try! MSSticker(contentsOfFileURL: url, localizedDescription: "Pasted Sticker")
+    return sticker;
+  }
+  
+  func fit(size: CGSize, dim: CGFloat) -> CGSize {
+    let scale = dim / max(size.width, size.height);
+    return CGSize(width: round(size.width * scale), height: round(size.height * scale))
+  }
+  
+  func createStickerFromPasteboard() {
+    let pb = UIPasteboard.general;
+    print("Pasteboard types:", pb.types, pb.changeCount)
+    
+    if (!pb.hasImages) {
+      infoLabel.isHidden = false
+      stickerView.isHidden = true
+      return
+    }
+    
+    guard var img = pb.image else { return }
+    
+    var basename = String(pb.changeCount);
+    
+    if let url = pb.url {
+      basename = NSString(string:url.lastPathComponent).deletingPathExtension
+    } else if let data = pb.data(forPasteboardType:"com.apple.mobileslideshow.asset.localidentifier") {
+      basename = String(decoding: data, as: UTF8.self).replacingOccurrences(of: "/", with: "_")
+    }
+    
+    let maxSize: CGFloat = 618
+    let midSize: CGFloat = 408
+    let minSize: CGFloat = 300
+    
+    var transparent = pb.contains(pasteboardTypes: ["public.png", "public.gif"])
+    
+    var scale = max(img.size.width, img.size.height) / midSize;
+    
+    if (scale > 1.0) {
+      let size = fit(size:img.size, dim: midSize)
+      img = img.resized(to: size)
+    }
+  
+    if (showBorder) {
+      transparent = true;
+      let distance = 16.0; //max(img.size.width, img.size.height) / 20;
+      img = img.stroked(with: .white, thickness: distance, quality: 10)
+      img = img.withShadow(blur: distance/2, offset: .init(width: 0, height: distance / 1.25), color: .init(white: 0.2, alpha: 0.333))
+      img = img.withShadow(blur: distance, offset: .init(width: 0, height: distance / 2), color: .init(white: 0.2, alpha: 0.15))
+    }
+    
+    var bytes = (transparent ? img.pngData() : img.jpegData(compressionQuality: 0.7))!.count;
+    if (bytes > 500000) {
+      print("shrinking", bytes)
+      let size = fit(size:img.size, dim: minSize)
+      img = img.resized(to: size)
+
+//      scale = scale * sqrt(2);
+//      img = img.resized(to: CGSize(width:img.size.width / scale, height:img.size.height / scale))
+    }
+    
+    print("image size", img.size.width, img.size.height)
+    bytes = (transparent ? img.pngData() : img.jpegData(compressionQuality: 0.7))!.count;
+
+    
+    let filename = "\(basename)\(showBorder ? "-border":"")\(transparent ? ".png" : ".jpg")"
+
+    if let oldFile = stickerFile { try? FileManager.default.removeItem(at:oldFile) }
+    stickerFile = createFile(image: img, filename: filename);
+    print("Writing", stickerFile as Any, img.isTransparent(), bytes)
+    let sticker = try? MSSticker(contentsOfFileURL: stickerFile!, localizedDescription: "Pasted Sticker");
+
+    if ((sticker) != nil) {
+      stickerView.sticker = sticker
+      stickerView.isHidden = false
+      toolbarView.isHidden = false
+      reloadButton.isHidden = true
+      infoLabel.isHidden = true
+    }
+  }
+  override func willBecomeActive(with conversation: MSConversation) {}
+  override func didBecomeActive(with conversation: MSConversation) {
+    createStickerFromPasteboard()
+  }
+  
+  func createFile(image: UIImage, filename: String) -> URL {
+    let fileManager = FileManager.default
+    do {
+      let documents = try fileManager.url(
+        for: .documentDirectory,
+        in: .userDomainMask,
+        appropriateFor: nil,
+        create: false
+      )
+      
+    let url = documents.appendingPathComponent(filename)
+      
+    let type = url.pathExtension
+    
+
+      if (type == "jpg") {
+        try image.jpegData(compressionQuality: 0.7)?.write(to: url)
+      } else {
+        try image.pngData()?.write(to: url)
+      }
+    return url;
+      
+    } catch { fatalError("Unable to create cache URL: \(error)") }
+    
+  }
+  
+  func createFile2(image: UIImage) -> URL {
+    //check if all image dimensions
+    //    let isImageDimensionValid = { (_ dimension:CGFloat)->Bool in return dimension >= 300 && dimension <= 618 }
+    
+    let cacheURL: URL
+    let fileManager = FileManager.default
+    let directoryName = UUID().uuidString
+    let tempPath = NSTemporaryDirectory()
+    
+    cacheURL = URL(fileURLWithPath: tempPath).appendingPathComponent(directoryName)
+    do { try fileManager.createDirectory(at: cacheURL, withIntermediateDirectories: true, attributes: nil) } catch { fatalError("Unable to create cache URL: \(error)") }
+    
+    let fileName = "image.png";
+    //    print("Length", image.jpegData(compressionQuality: 0.8)?.count ?? "?")
+    let url = cacheURL.appendingPathComponent(fileName)
+    try! image.pngData()?.write(to: url)
+    return url;
+  }
+  @IBAction func refreshUI(_ sender: UIButton) {
+    createStickerFromPasteboard();
+  }
+  
+  @IBAction func setSize(_ sender: UIButton) {
+    let size = sender.tag;
+    let d = (CGFloat(size) - stickerView.frame.size.width) / 2
+    self.stickerView.frame = self.stickerView.frame.insetBy(dx: -d, dy: -d)
+  }
+  
+  @IBAction func toggleBorder(_ sender: UIButton) {
+    showBorder = !showBorder;
+    store.set(showBorder, forKey: "showBorder")
+    store.synchronize();
+    createStickerFromPasteboard();
+  }
+  
+  override func didResignActive(with conversation: MSConversation) {
+  }
+  
+  override func didReceive(_ message: MSMessage, conversation: MSConversation) {
+  }
+  
+  override func didStartSending(_ message: MSMessage, conversation: MSConversation) {
+  }
+  
+  override func didCancelSending(_ message: MSMessage, conversation: MSConversation) {
+  }
+  
+  override func willTransition(to presentationStyle: MSMessagesAppPresentationStyle) {
+    if (presentationStyle == .expanded) {
+      stickerBrowser.isHidden = false
+      containerView.isHidden = true
+      stickerBrowser.dataSource = self
+      stickerBrowser.reloadData();
+    } else {
+      stickerBrowser.isHidden = true
+      containerView.isHidden = false
+    }
+    print(presentationStyle);
+  }
+  
+  override func didTransition(to presentationStyle: MSMessagesAppPresentationStyle) {
+  }
+  
+}
+
 
 extension UIImage {
   func resized(to size: CGSize) -> UIImage {
     return UIGraphicsImageRenderer(size: size).image { _ in
       draw(in: CGRect(origin: .zero, size: size))
     }
+  }
+  public func isTransparent() -> Bool {
+    guard let alpha: CGImageAlphaInfo = self.cgImage?.alphaInfo else { return false }
+    return alpha == .first || alpha == .last || alpha == .premultipliedFirst || alpha == .premultipliedLast
   }
   func withShadow(blur: CGFloat = 6, offset: CGSize = .zero, color: UIColor = UIColor(white: 0, alpha: 0.8)) -> UIImage {
     
@@ -165,107 +392,4 @@ extension CGPoint {
         let y = origin.y + radius * sin(newAzimuth)
         return CGPoint(x: x, y: y)
     }
-}
-
-class MessagesViewController: MSMessagesAppViewController {
-  @IBOutlet weak var stickerView: MSStickerView!
-  @IBOutlet weak var infoLabel: UILabel!
-  var showBorder = false;
-  
-  func updateUI() {
-    
-    let pb = UIPasteboard.general;
-    print("Pasteboard types:", pb.types)
-    
-    guard var content = UIPasteboard.general.image else {
-      infoLabel.isHidden = false;
-      stickerView.isHidden = true;
-      return
-    }
-    
-    let maxSize: CGFloat = 512
-    
-    
-    var scale = max(content.size.width, content.size.height) / maxSize;
-    
-    if (scale > 1.0) {
-      content = content.resized(to: CGSize(width:content.size.width / scale, height:content.size.height / scale))
-    }
-    
-    
-    if (showBorder) {
-      let distance = min(content.size.width, content.size.height) / 20;
-      content = content.stroked(with: .white, thickness: distance, quality: 10)
-      content = content.withShadow(blur: distance, offset: .init(width: 0, height: distance / 1.5), color: .init(white: 0.2, alpha: 0.333))
-    }
-    
-    let bytes = content.pngData()!.count;
-    if (bytes > 500000) {
-      scale = scale * 2;
-      content = content.resized(to: CGSize(width:content.size.width / scale, height:content.size.height / scale))
-    }
-    
-    let file = createFile(image: content)
-    let sticker = try? MSSticker(contentsOfFileURL: file, localizedDescription: "Pasted Sticker");
-    
-    if ((sticker) != nil) {
-      stickerView.sticker = sticker
-      stickerView.isHidden = false;
-      infoLabel.isHidden = true;
-    }
-  }
-  override func willBecomeActive(with conversation: MSConversation) {
-    updateUI()
-  }
-  
-  
-  
-  func createFile(image: UIImage) -> URL {
-    //check if all image dimensions
-    //    let isImageDimensionValid = { (_ dimension:CGFloat)->Bool in return dimension >= 300 && dimension <= 618 }
-    
-    let cacheURL: URL
-    let fileManager = FileManager.default
-    let directoryName = UUID().uuidString
-    let tempPath = NSTemporaryDirectory()
-    
-    cacheURL = URL(fileURLWithPath: tempPath).appendingPathComponent(directoryName)
-    do { try fileManager.createDirectory(at: cacheURL, withIntermediateDirectories: true, attributes: nil) } catch { fatalError("Unable to create cache URL: \(error)") }
-    
-    let fileName = "image.png";
-    //    print("Length", image.jpegData(compressionQuality: 0.8)?.count ?? "?")
-    let url = cacheURL.appendingPathComponent(fileName)
-    try! image.pngData()?.write(to: url)
-    return url;
-  }
-  
-  @IBAction func refreshUI(_ sender: UIButton) {
-    updateUI();
-  }
-  
-  @IBAction func toggleBorder(_ sender: UIButton) {
-    showBorder = !showBorder;
-    updateUI();
-  }
-  
-  
-  override func didResignActive(with conversation: MSConversation) {
-  }
-  
-  override func didReceive(_ message: MSMessage, conversation: MSConversation) {
-  }
-  
-  override func didStartSending(_ message: MSMessage, conversation: MSConversation) {
-  }
-  
-  override func didCancelSending(_ message: MSMessage, conversation: MSConversation) {
-  }
-  
-  override func willTransition(to presentationStyle: MSMessagesAppPresentationStyle) {
-    print(presentationStyle);
-  }
-  
-  override func didTransition(to presentationStyle: MSMessagesAppPresentationStyle) {
-  }
-  
 }
